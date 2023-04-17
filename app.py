a04ff36d665a62f60c6fc47c319302a225c09237
Mark Parrish <markparrish@gwu.edu>
2023-04-17 13:47:13 -0400

    added regs

--- app.py
+++ app.py
@@ -10,3 +10,39 @@ mydb = mysql.connector.connect(
   password = "marksheilazack",
   database = "university"
 )
+
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+  cursor = mydb.cursor(dictionary=True)
+
+  session['error'] = ''
+
+  if request.method == 'POST':
+    password = request.form['password']
+    session['uid'] = request.form['username']
+    
+    cursor.execute('SELECT * FROM users WHERE uid = %s', (session['uid'], ))
+    user_info = cursor.fetchone()
+    mydb.commit()
+    cursor.close()
+
+    if user_info is None:
+      session['error'] = 'That username is not valid'
+    elif user_info['password_hash'] == password:
+      session['uid'] = str(user_info['uid'])
+      session['first_name'] = user_info['first_name']
+      session['middle_initial'] = user_info['middle_initial']
+      session['last_name'] = user_info['last_name']
+      session['address'] = user_info['address']
+      session['birthday'] = user_info['birthday']
+      session['user_type'] = user_info['user_type']
+      return redirect('/Portal')
+    else:
+      session['error'] = 'That\'s the wrong password you silly goose'
+
+  return render_template('Login.html')
+
+@app.route("/logout")
+def logout():
+  session.clear()
+  return redirect(url_for("login"))
